27e273742ac13ea487559067670cca064c9935db
Aldo Adabunu <65352287+KwakuAldo@users.noreply.github.com>
2022-12-29 14:08:04 +0000

    building layout

--- script/index.js
+++ script/index.js
@@ -6,6 +6,12 @@ const removeBtn = document.getElementById('remove-btn');
 const cartBtn = document.getElementById('cart-btn');
 let itemQty = document.getElementById('item-quantity');
 let count = 0;
+const slides = document.querySelectorAll('.slide')
+let curSlide = 0
+const nextSlide = document.querySelector('.btn-next');
+const prevSlide = document.querySelector('.btn-prev');
+let maxSlide = slides.length -1
+
 
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('hidden');
@@ -25,6 +31,7 @@ addBtn.addEventListener('click', () => {
 
 removeBtn.addEventListener('click', () => {
     count -= 1;
+    /* To prevent the counter from going below zero */
     if (count < 0) {
     count = 0;
     }
@@ -35,4 +42,32 @@ removeBtn.addEventListener('click', () => {
 
 cartBtn.addEventListener('click', () => {
     
+})
+
+slides.forEach((slider, indx) => {
+    slider.style.transform = `translateX(${indx * 100}%)`
+})
+
+nextSlide.addEventListener('click', function () {
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++
+    }
+    console.log('next')
+    slides.forEach((slider, indx) => {
+        slider.style.transform = `translateX(${100 * (indx - curSlide)}%)`
+    })
+})
+
+prevSlide.addEventListener('click', function () {
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--
+    }
+    console.log('previous')
+    slides.forEach((slider, indx) => {
+        slider.style.transform = `translateX(${100 * (indx - curSlide)}%)`
+    })
 })
